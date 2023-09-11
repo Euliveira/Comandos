327bde899f52af420d2dd826ca63334bc62094f4
Deoliveira_oficial <97267995+Euliveira@users.noreply.github.com>
2023-09-11 12:56:01 -0300

    Create Install.sh
    Comandos essênciais de instalação para seu terminal

---
+++ Install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sleep 1
+echo "===================="
+echo " Willian de Oliveira"
+echo "===================="
+echo "Te da as boas vindas"
+sleep 1
+echo "Instalando comandos essenciais"
+sleep 4
+
+pkg install upgrade
+pkg install update
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install tor
+pkg install figlet
+pkg install telethon
+pkg install php
+pkg install curl
+apt install git
+pip install clang
+pip install socket
+apt install java
+pkg install colorama
+pkg install proxychains-ng
+pkg install nmap
+pkg install ping
+pkg install host
+apt install coreutils
+apt install git node ruby
+apt install cowsay
+cowsay "Ola, programador"
+sleep 2
+termux-setup-storage
